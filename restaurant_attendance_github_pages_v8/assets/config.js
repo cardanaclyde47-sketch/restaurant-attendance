428c8953e731ce90a32d6d1fcf633111bfa3b0e6
cardanaclyde47-sketch <cardanaclyde47@gmail.com>
2026-06-12 18:33:36 +0800

    Update config.js

--- restaurant_attendance_github_pages_v8/assets/config.js
+++ restaurant_attendance_github_pages_v8/assets/config.js
@@ -1,27 +1,17 @@
-// Firebase config goes here. This version is GitHub Pages ready.
-// For now, the app works in LOCAL DEMO MODE if you leave these placeholders.
-// To make it truly online across laptop/admin/employee phones, paste your Firebase Web App config here.
-window.firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAloCEFwBOsDAZ5vXxSxCLpHTc_f7NdJWs",
+  authDomain: "restaurant-attendance-a1cd2.firebaseapp.com",
+  projectId: "restaurant-attendance-a1cd2",
+  storageBucket: "restaurant-attendance-a1cd2.firebasestorage.app",
+  messagingSenderId: "794903663053",
+  appId: "1:794903663053:web:63c9734212424bcad5b484"
 };
 
-window.APP_CONFIG = {
-  restaurantName: "Restaurant Attendance",
-  kioskCode: "RESTO123",
-  qrSeconds: 30,
-  demoAdmin: {
-    name: "Admin Owner",
-    email: "admin@restaurant.local",
-    password: "admin123"
-  },
-  demoEmployee: {
-    name: "Juan Employee",
-    email: "juan@restaurant.local",
-    password: "employee123"
-  }
-};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
